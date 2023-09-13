da2f37b5578964def22d75421d3f44e62135df4e
RossShow <rossshow11@gmail.com>
2023-09-13 09:45:23 -0400

    Finished the prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        string first = "";
+        string last = "";
+
+
+        Console.Write("What is your first name? ");
+        first = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        last = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last}, {first} {last}");
+
+
     }
 }
