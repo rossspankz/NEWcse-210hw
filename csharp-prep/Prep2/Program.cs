3a5cd458c5fbe0b66da50883605ff91bc3b34af6
RossShow <rossshow11@gmail.com>
2023-09-13 14:20:48 -0400

    Almost done just needs a few things

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        String num = "0";
+        
+
+
+        Console.Write("What is your grade? ");
+        x = Console.ReadLine();
+
+        if (x >= 90)
+        {
+            Console.WriteLine("You got an A!");
+        }
+        else if (x >= 80)
+        {
+            Console.WriteLine("You got an B!");
+        }
+        else if (x >= 70)
+        {
+            Console.WriteLine("You got an C :(");
+        }
+        else if (x >= 60)
+        {
+            Console.WriteLine("You got an D");
+        }
+        else if (x < 60)
+        {
+            Console.WriteLine("You got an F");
+        }
     }
 }
