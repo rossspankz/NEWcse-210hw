11a86fc12dbaf50fc35c284e294967b0f74d52ce
rossspankz <rossshow11@gmail.com>
2023-09-20 17:55:00 -0600

    For some reason the else if isn't working

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic Number?");
+        int magicNumber = int.Parse(Console.ReadLine());
+
+        int gue = 0;
+
+       while(magicNumber != gue)
+        {
+            Console.Write("What is your guess?");
+            gue = int.Parse(Console.ReadLine());
+
+            if ( magicNumber > gue)
+            {
+                Console.WriteLine("Higher");
+            }
+            
+            else if (magicNumber < gue)
+            {
+                Console.WriteLine("Lower");
+            }
+         
+            else
+           {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
