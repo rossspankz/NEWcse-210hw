754e9ebe9c7ab92dca66656066e9330343d8e946
RossShow <rossshow11@gmail.com>
2023-09-20 21:44:05 -0400

    Can't get the positive minimum to work

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,53 @@
 using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int num = -1;
+        int sum = 0;
+        float avNum = 0;
+        
+        List<int> listNum = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished");
+        
+        while(num != 0)
+        {
+            Console.Write("Enter number: ");
+            num = int.Parse(Console.ReadLine());
+
+            if (num !=0)
+            {
+            listNum.Add(num);
+            }
+        }
+        // The sum
+        sum = listNum.Sum();
+        Console.WriteLine($"The sum is {sum}");
+
+        // The average
+        avNum = sum / listNum.Count();
+        Console.WriteLine($"The average is {avNum}");
+
+        //The maximum
+        int max = listNum.Max();
+        Console.WriteLine($"The maximum is {max}");
+
+        // The min
+        int min = listNum[0];
+        foreach (int minNum in listNum)
+        {
+            if (min > 0 && minNum < min)
+            {
+               min = minNum;
+            }
+        }
+        Console.WriteLine($"the minimum is {min}");
+
     }
 }
