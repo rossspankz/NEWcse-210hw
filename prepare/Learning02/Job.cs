d5362d5b93941d3716e4344b2857c4ee1073df49
rossspankz <rossshow11@gmail.com>
2023-09-27 21:20:08 -0600

    Swag

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -1,23 +1,13 @@
+using System;
+
 public class Job
 {
-    public string _company = "";
-    public string _jobTitle = "";
+    public string _company;
+    public string _jobTitle;
     public int _startYear;
     public int _endYear;
     public void showJob()
     {
-        Console.Write("What was your job title? ");
-        _jobTitle = Console.ReadLine();
-
-        Console.Write("What comapny did you work for? ");
-        _company = Console.ReadLine();
-
-        Console.Write("What year did you start? ");
-        _startYear = int.Parse(Console.ReadLine());
-
-        Console.Write("What year did you stop working there? ");
-        _endYear = int.Parse(Console.ReadLine());
-
         Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
     }
     
