758a4e1e7bfc561871e6a3059999dd73cdc7ce28
rossspankz <rossshow11@gmail.com>
2023-09-27 21:18:54 -0600

    All the job part done

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,7 +5,15 @@ class Program
     static void Main(string[] args)
     {
         Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
         Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
 
         job1.showJob();
         job2.showJob();
